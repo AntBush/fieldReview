643b1099599fd250da25cc072ac73c90527cb103
antbush <tonybushara@gmail.com>
2020-09-05 19:13:00 -0400

    Changed gradle stuff and started the docx builder

--- src/main/java/xyz/anthony/fieldReview/DocxBuilder.java
+++ src/main/java/xyz/anthony/fieldReview/DocxBuilder.java
@@ -1,23 +1,47 @@
 package xyz.anthony.fieldReview;
 
+import java.io.File;
+
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.HeaderPart;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Hdr;
+import org.docx4j.wml.ObjectFactory;
+import org.docx4j.wml.P;
+import org.docx4j.wml.Text;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
-public class DocxBuilder {
-    private WordprocessingMLPackage wordMLPackage;
-    
+public class DocxBuilder  {
+	private WordprocessingMLPackage wordMLPackage;
+	private ObjectFactory objectFactory = new ObjectFactory();
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public void doShit(){
-        System.out.println("");
-        try{
-            wordMLPackage = WordprocessingMLPackage.createPackage();
-            
 
-        }catch (Exception e){
+	public void doShit(){
+		try{
+			logger.info("Creating Word package");
+			wordMLPackage = WordprocessingMLPackage.createPackage();
+			logger.info("Error here?");
+			MainDocumentPart mdp = wordMLPackage.getMainDocumentPart();
+			HeaderPart hdrPart = new HeaderPart();
+			Hdr header = objectFactory.createHdr();
+			Text text = objectFactory.createText();
 
-        }
-    }
+			text.setValue("asdf");
+			header.getContent().add(text);
+			hdrPart.setContents(header);
+			mdp.addTargetPart(new HeaderPart());
+			File exportFile = new File("welcome.docx");
+			wordMLPackage.save(exportFile);
+			
+		}catch (Exception e){
+			logger.error(e.getMessage());
+			
+		}
+	}
 
 }
---
+++ src/main/java/xyz/anthony/fieldReview/TestEndpoint.java
@@ -0,0 +1,28 @@
+package xyz.anthony.fieldReview;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class TestEndpoint {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping(value="/test")
+    public String getMethodName() {
+        
+        logger.info("Instantiating doc class");
+        DocxBuilder doc = new DocxBuilder();
+
+        doc.doShit();
+        
+        return "Test";
+    }
+    
+}
